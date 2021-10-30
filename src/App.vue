0e079cb08b1f25df89e677641cd9405232c41f43
lmartinezs <martinez.s.luis@gmail.com>
2021-10-30 09:22:35 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import BaseSwitch from "@/components/BaseSwitch.vue";
+
+export default {
+  components: { BaseSwitch }
+};
+</script>
+
+<template>
+  <div class="py-16">
+    <BaseSwitch />
+  </div>
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BaseSwitch.vue
@@ -0,0 +1,57 @@
+<script>
+import { Switch } from "@headlessui/vue";
+import { ref } from "vue";
+
+export default {
+  name: "BaseSwitch",
+  components: { Switch },
+  setup() {
+    const enabled = ref(false);
+
+    return { enabled };
+  },
+};
+</script>
+
+<template>
+  <Switch
+    v-model="enabled"
+    :class="enabled ? 'bg-yellow-900' : 'bg-yellow-700'"
+    class="
+      relative
+      inline-flex
+      flex-shrink-0
+      h-[38px]
+      w-[74px]
+      border-2 border-transparent
+      rounded-full
+      cursor-pointer
+      transition-colors
+      ease-in-out
+      duration-200
+      focus:outline-none
+      focus-visible:ring-2
+      focus-visible:ring-white
+      focus-visible:ring-opacity-75
+    "
+  >
+    <span class="sr-only">Use setting</span>
+    <span
+      aria-hidden="true"
+      :class="enabled ? 'translate-x-9' : 'translate-x-0'"
+      class="
+        pointer-events-none
+        inline-block
+        h-[34px]
+        w-[34px]
+        rounded-full
+        bg-white
+        shadow-lg
+        ring-0
+        transition
+        ease-in-out
+        duration-200
+      "
+    />
+  </Switch>
+</template>
